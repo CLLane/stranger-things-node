50536401f386d3f277bde2233056762e82f93218
Chris Lane <pankokrumbs@gmail.com>
2020-01-26 00:07:02 -0700

    Cleaning data complete

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fetch = require('node-fetch')
 const app = express();
-const port = 3000;
+const port = 3001;
 const url = 'http://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes'
 
 let data = async (url) => {
@@ -14,10 +14,60 @@ let data = async (url) => {
 }
 }
 
+calculateSeriesDuration = data => {
+  let episodes = data._embedded.episodes;
+  return episodes.reduce((totalSec, currentEpisode) => {
+    totalSec = totalSec + currentEpisode.runtime * 60;
+    return totalSec;
+  }, 0);
+};
+getAvgEpisodes = data => {
+  let episodes = data._embedded.episodes;
+  let seasons = episodes.map(currentEpisode => currentEpisode.season);
+  return calculateAvgEpisodes(seasons);
+};
+
+calculateAvgEpisodes = array => {
+  let seasons = array.reduce((acc, cv) => {
+    if (!acc.includes(cv)) {
+      acc.push(cv)
+    }
+    return acc;
+  }, [])
+
+  let totalEpisodes = array.length;
+  return totalEpisodes / seasons.length
+};
+
+cleanEpisodes = (data) => {
+  let episodes = data._embedded.episodes;
+  let episodeInfoArray = episodes.map(episode => {
+    let summary = `${episode.summary}`
+    let formattedSummary = `${summary.split('<p>').join('').split('</p>').join('').split('.')[0]}.`
+    return {
+      [episode.id]: {
+        sequenceNumber: `s${episode.season}e${episode.number}`,
+        shortTitle: `${episode.name.split(': ')[1]}`,
+        airTimestamp: `${episode.airstamp}`,
+        shortSummary: `${formattedSummary}`
+      }
+    }
+  })
+  return episodeInfoArray;
+}
+
 app.get('/', async (request, response) => {
-  let y = await data(url)
-  console.log(y)
-  response.status(200).send(y)
+  let fetchResponse = await data(url)
+  let seriesDuration = calculateSeriesDuration(fetchResponse);
+  let avgEpisodes = getAvgEpisodes(fetchResponse);
+  let episodes = cleanEpisodes(fetchResponse);
+
+  response.status(200).send({
+    cleanedStrangerData: {
+      totalDurationSec: seriesDuration,
+      averageEpisodesPerSeason: avgEpisodes,
+      episodes,
+    }})
 })
 
 app.listen(port, (err) => {
