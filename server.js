8986d7a1eabbfa97f61b33034468851e34b88edd
Chris Lane <pankokrumbs@gmail.com>
2020-01-25 21:35:10 -0700

    Server testing

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const fetch = require("node-fetch");
+const http = require('http');
+const server = http.createServer();
 
 const url = "http://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes";
 
+server.listen(3001, () => {
+  console.log('The HTTP server is listening at Port 3001')
+})
+
+server.on('request', (request, response) => {
+  if (request.method === 'GET') {
+    return getData(url)
+  }
+})
+
+// const app = express();
+// const environment = process.env.NODE_ENV || 'development'
 const getData = async url => {
   try {
     const response = await fetch(url);
@@ -12,4 +26,3 @@ const getData = async url => {
   }
 };
 
-getData(url);
