7103473412a67bf89b08903345eea9b11ac1646f
Chris Lane <pankokrumbs@gmail.com>
2020-01-25 21:13:17 -0700

    init commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const fetch = require("node-fetch");
+
+const url = "http://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes";
+
+const getData = async url => {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+ggetData(url);
